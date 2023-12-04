4fbe9907a68d2bd8c52b637f33e744aedd65e63f
Nicolas-Centurion <Kikiparavos@gmail.com>
2023-12-04 15:09:25 -0300

    Terminado homework M03 M04 y parcial M05 prepcourse viejo

---
+++ M04 JavaScript Bucles/prueba.js
@@ -0,0 +1,11 @@
+
+
+
+console.log (3 == 3); 
+console.log (3 === 3); 
+console.log (3 == "3"); 
+console.log (3 === "3");
+console.log (7 == "7");
+console.log (7 === "7");
+
+console.log("Hola Kiki")
